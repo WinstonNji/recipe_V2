4b11fe000c2a57ead5d739991e1851405a85cdda
Winston Nji <winston00russel@gmail.com>
2025-03-11 16:13:19 +0200

    removed white bg

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 function Favourites() {
   return (
-    <div className='bg-red-500 h-screen flex flex-1 justify-center p-10 box-border'>
+    <div className='h-screen flex flex-1 justify-center p-10 box-border'>
         
     </div>
   )
